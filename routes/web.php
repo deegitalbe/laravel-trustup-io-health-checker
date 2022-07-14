5bcf862c03053b96dd4ca001e0814108e2c23959
romainvausetrustup <romain.vause@trustup.be>
2022-07-14 13:25:11 +0200

    Updated the url path to access to the health checks

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use Spatie\Health\Http\Controllers\HealthCheckJsonResultsController;
 use Spatie\Health\Http\Controllers\HealthCheckResultsController;
 
 //Route::get('health-dashboard', HealthCheckResultsController::class);
-Route::get('health-json', HealthCheckJsonResultsController::class);
+Route::get('trustup-io/health/json', HealthCheckJsonResultsController::class);
